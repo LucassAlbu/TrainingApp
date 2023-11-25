2ad94759bc009d096caf49e790adb2438b7b92ac
LucassAlbu <lucassilva.alb@hotmail.com>
2023-11-25 11:25:19 -0300

    first commit

---
+++ app/src/main/java/com/albuquerque/trainingapp/presentation/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.albuquerque.trainingapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.albuquerque.trainingapp.R
+import com.albuquerque.trainingapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var navController: NavController
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+    }}
